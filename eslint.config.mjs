cb64105da90e42bbbf94e750750f8092dc1e5a88
Brandon Hernández <isahecam@gmail.com>
2025-11-14 23:44:36 -0600

    Enhance ESLint configuration with Prettier integration and custom rules for import ordering and JSX prop sorting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,68 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier/flat";
+import eslintPluginImport from "eslint-plugin-import";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginReact from "eslint-plugin-react";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettierConfig, // Disable ESLint conflicts rules with Prettier
+
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+      react: eslintPluginReact,
+      import: eslintPluginImport,
+    },
+
+    // Custom rules
+    rules: {
+      "prettier/prettier": ["error"],
+
+      // Maximum number of consecutive empty lines to 1
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+
+      // Rule to order imports
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "type",
+            "builtin",
+            "object",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          pathGroups: [
+            {
+              pattern: "@/*",
+              group: "external",
+              position: "after",
+            },
+          ],
+          "newlines-between": "always",
+        },
+      ],
+
+      // Order props in JSX or TSX
+      "react/jsx-sort-props": [
+        "error",
+        {
+          callbacksLast: true,
+          shorthandFirst: true,
+          reservedFirst: true,
+          noSortAlphabetically: true,
+        },
+      ],
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
