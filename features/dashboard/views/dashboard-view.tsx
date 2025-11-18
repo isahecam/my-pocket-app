42ae6b29790b4a63319be2c5f29ac81ae97644bb
Brandon Hernández <isahecam@gmail.com>
2025-11-17 20:26:07 -0600

    feat: create DashboardView component to structure the main dashboard layout

---
+++ features/dashboard/views/dashboard-view.tsx
@@ -0,0 +1,11 @@
+import { FinancialSummarySection } from "@/features/dashboard/sections/financial-summary";
+
+export async function DashboardView() {
+  return (
+    <main className="@container/main flex flex-1 flex-col gap-2 p-6">
+      <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+        <FinancialSummarySection />
+      </div>
+    </main>
+  );
+}
