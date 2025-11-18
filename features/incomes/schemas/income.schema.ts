1c47182bd836e3638965b9818b7755609144f4ae
Brandon Hernández <isahecam@gmail.com>
2025-11-17 20:26:44 -0600

    feat: create schema for income entry validation

---
+++ features/incomes/schemas/income.schema.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+import { incomeSchema } from "./base.schema";
+
+export const createIncomeSchema = incomeSchema;
+
+export type CreateIncome = z.infer<typeof createIncomeSchema>;
