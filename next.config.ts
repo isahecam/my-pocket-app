426d56dd6573b4c042f3a1b58fe3572c7d2ae035
Brandon Hernández <isahecam@gmail.com>
2025-11-28 22:51:18 -0600

    chore: update Next.js configuration to enable typed routes and experimental typed environment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typedRoutes: true,
   cacheComponents: true,
+  experimental: {
+    typedEnv: true,
+  },
 };
 
 export default nextConfig;
