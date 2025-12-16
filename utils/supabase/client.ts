701be8fc5691c9dbd718af60c1bdd3da92e2e86a
Brandon Hernández <isahecam@gmail.com>
2025-12-15 19:30:36 -0600

    feat: Add Supabase client utilities for browser and server-side rendering.

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,10 +1,14 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+export function createClient() {
+  // Create a supabase client on the browser with project's credentials
 
-if (!(supabaseUrl && supabaseKey)) {
-  throw new Error("Missing Supabase environment variables");
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+  if (!(supabaseUrl && supabaseKey)) {
+    throw new Error("Missing Supabase environment variables");
+  }
+
+  return createBrowserClient(supabaseUrl, supabaseKey);
 }
-
-export const createClient = () => createBrowserClient(supabaseUrl, supabaseKey);
